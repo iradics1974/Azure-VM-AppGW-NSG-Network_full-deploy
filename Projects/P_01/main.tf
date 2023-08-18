4ed3dbae0358622c6e2a2dfac3d6b1a7a54350cd
iradics1974 <75294569+iradics1974@users.noreply.github.com>
2023-08-18 17:35:58 +0200

    Update main.tf

--- Projects/P_01/main.tf
+++ Projects/P_01/main.tf
@@ -9,7 +9,7 @@ module "my_vnet" {
   address_space       = ["10.0.0.0/16"]
   subnet_count        = 2
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
-  resource_group_name = "myTFResourceGroup"
+  resource_group_name = "my-resource-group"
   location            = "East US"
 }
 
